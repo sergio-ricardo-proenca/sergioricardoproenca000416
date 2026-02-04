df62d14721fdf1c8977ea45667a7bd7cda1702a7
Sergio Ricardo Proenca <sergio.rproenca@gmail.com>
2026-02-04 09:03:08 -0400

    feat: implementa service de upload e endpoint de cadastro de albuns com capa

--- src/main/java/com/sergioproenca/desafio_backend/controller/AlbumController.java
+++ src/main/java/com/sergioproenca/desafio_backend/controller/AlbumController.java
@@ -2,26 +2,42 @@ package com.sergioproenca.desafio_backend.controller;
 
 import com.sergioproenca.desafio_backend.model.Album;
 import com.sergioproenca.desafio_backend.repository.AlbumRepository;
+import com.sergioproenca.desafio_backend.service.CapaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/v1/albuns") // Adicionado /v1 para atender ao requisito (j) de versionamento
+@RequestMapping("/api/v1/albuns")
 public class AlbumController {
 
     @Autowired
     private AlbumRepository repository;
 
-    @GetMapping
-    public Page<Album> listarTodos(@PageableDefault(size = 10) Pageable paginacao) {
-        return repository.findAll(paginacao);
-    }
+    @Autowired
+    private CapaService capaService;
 
     @PostMapping
-    public Album cadastrar(@RequestBody Album album) {
-        return repository.save(album);
+    public ResponseEntity<Album> salvar(
+            @RequestParam("titulo") String titulo, 
+            @RequestParam("artista_id") Long artistaId,
+            @RequestParam("capa") MultipartFile capa) {
+        
+        try {
+            // 1. Envia a foto para o MinIO (Requisito h)
+            String nomeFoto = capaService.enviarCapa(capa);
+
+            // 2. Cria o objeto Álbum usando os métodos corretos do seu Model (Requisito g)
+            Album album = new Album();
+            album.setTitulo(titulo); 
+            album.setCapaUrl(nomeFoto); 
+
+            return ResponseEntity.ok(repository.save(album));
+            
+        } catch (Exception e) {
+            e.printStackTrace(); // Isso ajuda a ver erros no console se o upload falhar
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
